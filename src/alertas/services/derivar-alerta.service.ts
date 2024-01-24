0c22143e51aa2789a10ae9b68bb4e0c0fcd88652
Juan <juantasayco266@gmail.com>
2024-01-24 09:40:27 -0500

    addLogicFilter

--- src/alertas/services/derivar-alerta.service.ts
+++ src/alertas/services/derivar-alerta.service.ts
@@ -1,10 +1,19 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { FILTER_DERIVAR_DATA } from '../data/derivar-alert.data';
+import { getDerivarAlerta } from '../dto/derivar-alerta/get-derivar-alerta.dto';
 
 @Injectable()
 export class DerivarAlertaService {
-  getDerivarAlerta(derivarParams: any) {
-    return FILTER_DERIVAR_DATA;
+  getDerivarAlerta(derivarParams: getDerivarAlerta) {
+    const { size, page } = derivarParams;
+
+    const dataAlert = JSON.parse(JSON.stringify(FILTER_DERIVAR_DATA));
+
+    dataAlert.data.delegations = dataAlert.data.delegations.slice(0, size);
+    dataAlert.data.numberItems = size;
+    dataAlert.data.numberPages = page;
+    return dataAlert;
   }
   createDerivarAlerta(bodyCreateDerivarAlerta: any) {
     return {
